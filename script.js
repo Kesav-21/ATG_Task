051c838b42105d9ec5ef9328775bdddea2230e50
Kesav-21 <cgkesav@gmail.com>
2023-10-16 09:42:55 +0530

    Functionalities and Sign In Added

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+var regModal=document.getElementById("registerModal");
+var logModal=document.getElementById("signInModal");
+
 function moreBtn() {
     document.getElementById("btn1").classList.toggle("btn-dark-subtle");
     document.getElementById("myDropdown").classList.toggle("show");
   }
+
 function locationEdit(){
     document.getElementById('locationInput').value="";
 }
@@ -16,16 +20,56 @@ function followBtn(){
     document.getElementById('follow1').classList.toggle('text-white')
 }
 
-function userBtn(){
-    var modal = document.getElementById("myModal");
-    var span = document.getElementsByClassName("close")[0];
-    modal.style.display="block";
-    
-}
-window.onclick = function(event) {
-    var modal = document.getElementById("myModal");
-    console.log("hello")
-  if (event.target == modal) {
-    modal.style.display = "none";
+function userRegister(){
+  if(logModal.classList.contains('d-block')){
+    regModal.classList.toggle('d-block')
+    logModal.classList.toggle('d-block')
   }
+  else
+  regModal.classList.toggle('d-block')
+}
+
+function userSignIn(){
+  if(regModal.classList.contains('d-block')){
+    regModal.classList.toggle('d-block')
+    logModal.classList.toggle('d-block')
+  }
+  else
+  logModal.classList.toggle('d-block')
+}
+
+function closeBtn(){
+  var regModal = document.getElementById("registerModal");
+  var logModal=document.getElementById("signInModal");
+  if(regModal.classList.contains('d-block'))
+  regModal.classList.toggle('d-block')
+  else
+  logModal.classList.toggle('d-block')
+}
+
+window.onclick = function(event) {
+  if (event.target == regModal) {
+    regModal.classList.toggle('d-block')
+  }
+  else if(event.target==logModal){
+    varModal.classList.toggle('d-block')
+  }
+}
+
+function joinGrp(){
+  var image=document.getElementById('joingrp')
+  var text=document.getElementById('grp-text')
+  var grpBtn=document.getElementById('grpBtn')
+  if(text.innerText=="Join Group"){
+    text.innerText="Leave Group"
+    image.src="/assets/images/leave.svg"
+    grpBtn.classList.toggle('btn-primary')
+    grpBtn.classList.toggle('btn-outline-secondary')
+}
+else{
+    text.innerText="Join Group"
+    image.src="/assets/images/join.svg"
+    grpBtn.classList.toggle('btn-outline-secondary')
+    grpBtn.classList.toggle('btn-primary')
+}
 }
